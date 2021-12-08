40252d85891df420ebe4bca132940426b8f3e6b4
Ilya Kostrikov <ikostrikov@gmail.com>
2021-12-08 11:53:37 -0800

    Apply formatting and fix bugs with faces parsing.

--- convert_obj_to_mujoco_msh.py
+++ convert_obj_to_mujoco_msh.py
@@ -1,5 +1,5 @@
-import struct
 import os
+import struct
 import sys
 
 if __name__ == "__main__":
@@ -19,18 +19,18 @@ if __name__ == "__main__":
             continue
 
         if arr[0] == 'v':
-            assert len(arr)==4
+            assert len(arr) == 4
             v.append([float(arr[i]) for i in range(1, 4)])
         elif arr[0] == 'vt':
-            assert len(arr) == 3
+            assert len(arr) == 4
             vt.append([float(arr[i]) for i in range(1, 3)])
         elif arr[0] == 'vn':
             assert len(arr) == 4
             vn.append([float(arr[i]) for i in range(1, 4)])
         elif arr[0] == 'f':
-                tmp = [arr[i].split("/") for i in range(1,len(arr))]
-                for i in range(1, len(tmp)-1):
-                    f.append([tmp[0], tmp[i], tmp[i+1]])
+            tmp = [arr[i].split("/") for i in range(1, len(arr))]
+            for i in range(1, len(tmp) - 1):
+                f.append([tmp[0], tmp[i], tmp[i + 1]])
         else:
             print(arr)
 
@@ -39,17 +39,25 @@ if __name__ == "__main__":
     output_vt = []
     output_f = []
 
-
     for i in f:
         assert len(i) == 3
         for j in i:
-            v_idx = int(j[0])-1
-            vt_idx = int(j[1]) - 1
-            vn_idx = int(j[2]) - 1
+            v_idx = int(j[0]) - 1
             output_v.append(v[v_idx])
-            output_vt.append(vt[vt_idx])
-            output_vn.append(vn[vn_idx])
-        output_f.append([len(output_v)-3, len(output_v)-2, len(output_v)-1])
+            try:
+                vt_idx = int(j[1]) - 1
+                output_vt.append(vt[vt_idx])
+            except:
+                pass
+            try:
+                vn_idx = int(j[2]) - 1
+                output_vn.append(vn[vn_idx])
+            except:
+                pass
+        output_f.append(
+            [len(output_v) - 3,
+             len(output_v) - 2,
+             len(output_v) - 1])
 
     print("len(v)=", len(output_v))
     print("len(vn)=", len(output_vn))
@@ -68,7 +76,6 @@ if __name__ == "__main__":
         assert type(input) == float
         return struct.pack(float_packing, input)
 
-
     out.write(int_to_bytes(len(output_v)))
     out.write(int_to_bytes(len(output_vn)))
     out.write(int_to_bytes(len(output_vt)))
@@ -82,13 +89,13 @@ if __name__ == "__main__":
             out.write(float_to_bytes(i[j]))
     for i in output_vt:
         out.write(float_to_bytes(i[0]))
-        out.write(float_to_bytes(1-i[1]))
+        out.write(float_to_bytes(1 - i[1]))
     for i in output_f:
         for j in range(3):
             out.write(int_to_bytes(i[j]))
     out.close()
     print("actual file size=", os.path.getsize(msh_f_name))
 
-    expected_file_size = 16 + 12*(len(output_v) + len(output_vn) + len(output_f)) + 8*len(output_vt)
+    expected_file_size = 16 + 12 * (len(output_v) + len(output_vn) +
+                                    len(output_f)) + 8 * len(output_vt)
     print("expected_file_size=", expected_file_size)
-
